19f6151770abb814efbdb82ec13d5e55684e740f
khrissh1 <khris.sh@i.ua>
2015-03-16 21:13:29 +0200

    First Test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * Created by Khrystyna.Shyian on 13.03.2015.
+ */
+public class FirstTest {
+
+    public static final By searchField = (By.id("lst-ib"));
+    public static boolean urlFound = false;
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new FirefoxDriver();
+    }
+
+    @DataProvider
+    public Object[][] keyWords() {
+        return new Object[][]{
+                new Object[]{"осциллограф", "www.tehencom.com"},
+        };
+    }
+
+    @Test(dataProvider = "keyWords")
+    public void firstTest(String keyWord, String url) throws InterruptedException {
+
+        driver.get("https://www.google.com.ua");
+        driver.findElement(searchField).clear();
+        driver.findElement(searchField).sendKeys(keyWord);
+        driver.findElement(searchField).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+
+        for (int i = 3; i <= 5; i++) {
+            System.out.println(i + "first");
+            Thread.sleep(2000);
+            List<WebElement> elementsList = driver.findElements(By.cssSelector("._Rm"));
+            Thread.sleep(2000);
+            for (WebElement option : elementsList) {
+                if (option.getText().contains(url)) {
+                    urlFound = true;
+                    break;
+                }
+            }
+            System.out.println(i + "second");
+            driver.findElement(By.xpath(".//*[@id='nav']/tbody/tr/td[" + i + "]/a/span")).click();
+        }
+        Assert.assertTrue(urlFound);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+}
